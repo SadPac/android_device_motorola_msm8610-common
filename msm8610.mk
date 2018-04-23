cf61d2883fa5a16aa87e3c014e752f10128fc9d4
Sushant Kumar <rahul.kr.sushant@gmail.com>
2018-04-23 16:06:54 +0000

    board edits for cam changes

--- msm8610.mk
+++ msm8610.mk
@@ -130,8 +130,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    #android.hardware.camera.provider@2.4-impl-legacy \
-    #camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8610 \
